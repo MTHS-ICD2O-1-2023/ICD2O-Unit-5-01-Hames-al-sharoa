cd7991e1195b8ef95d10798bd6141b6d6e3c14e8
Hames.Al-Sharoa <159185115+Hames-al-sharoa@users.noreply.github.com>
2024-06-04 18:14:38 +0000

    save all changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,40 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 hames al sharoa All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: hames al sharoa
+// Created on: may 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS20-Unit5-01-JS/sw.js", {
+    scope: "/ICS20-Unit5-01-JS/",
+  })
+}
+
+const randomNumber = Math.floor(Math.random() * 6) + 1
+
+/**
+ * This function updates the slider value.
+ */
+function updateSliderValue(valueFromSlider) {
+  document.getElementById("slider-value").innerHTML = valueFromSlider
+}
+
+function guess() {
+  const valueFromSlider = parseInt(document.getElementById("slider").value)
+
+  // process
+  if (valueFromSlider == randomNumber) {
+    document.getElementById("answer").innerHTML =
+      "Ding! Ding! Ding! You got it! The answer was " + randomNumber + ". :D"
+  }
+
+  if (valueFromSlider != randomNumber) {
+    document.getElementById("answer").innerHTML =
+      "Err...not quite. Try again. :("
+  }
+}
